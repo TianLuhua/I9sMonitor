3f0de5515a64af45a1fbeac8885894647df25480
tianluhua <466469918@qq.com>
2018-10-26 16:43:31 +0800

    add bugly

--- app/src/main/java/com/MyApp.java
+++ app/src/main/java/com/MyApp.java
@@ -3,9 +3,12 @@ package com;
 import android.app.Application;
 import android.content.Context;
 
+import com.tencent.bugly.crashreport.CrashReport;
 import com.tencent.util.LoggerUtils;
 
 import java.io.File;
+
+import static com.booyue.Constant.Bugly.APP_ID;
 
 /**
  * Created by Administrator on 2017/5/26.
@@ -16,6 +19,8 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //初始化Bugly
+        CrashReport.initCrashReport(getApplicationContext(), APP_ID, false);
         instance = this;
         File file = new File(getFilesDir(), "debug.txt");
         if (file.exists()) {
--- app/src/main/java/com/booyue/Constant.java
+++ app/src/main/java/com/booyue/Constant.java
@@ -9,4 +9,11 @@ public class Constant {
     public static final int RECEIVE_WAIT = 2;//
     public static final int AUDIO_COMMUNICATING_ = 3;//语音通话中
 
+    /**
+     * Tencen Bugly相关参数
+     */
+    public static class Bugly {
+        public static final String APP_ID = "9824f36e23";
+    }
+
 }
