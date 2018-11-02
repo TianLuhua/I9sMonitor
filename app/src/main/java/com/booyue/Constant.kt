6830954191083726e1e9c8842a54ea30d95f72e1
tianluhua <466469918@qq.com>
2018-11-02 16:51:46 +0800

    将系统的配置属性改成顶层属性形式

--- app/src/main/java/com/booyue/Constant.kt
+++ app/src/main/java/com/booyue/Constant.kt
@@ -3,17 +3,7 @@ package com.booyue
 /**
  * Created by Tianluhua on 2018\11\2 0002.
  */
-class Constant {
-
-    companion object {
-        val SEND_WAIT = 1
-        val RECEIVE_WAIT = 2//
-        val AUDIO_COMMUNICATING_ = 3//语音通话中
-    }
-
-    class Bugly {
-        companion object {
-            val APP_ID = "9824f36e23"
-        }
-    }
-}
+const val APP_ID = "9824f36e23"
+const val SEND_WAIT = 1
+const val RECEIVE_WAIT = 2
+const val AUDIO_COMMUNICATING_ = 3
